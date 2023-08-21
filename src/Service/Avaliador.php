a3c9482b8157781500e348527a6eefa2e646191f
IgorTelesCaetano <igor-t-c@hotmail.com>
2023-08-21 17:45:13 -0300

    O PHPUnit é uma ferramenta para executar testes de maneira automatizada O executável do PHPUnit se encontra na pasta vendor/bin Para escrever um teste com PHPUnit, devemos criar uma classe de teste Uma classe de teste segue algumas regras: Começa com o nome da classe que está sendo testada e usa o sufixo Test, por exemplo: AvaliadorTest, em geral ClasseASerTestadaTest A classe de teste deve estender a classe TestCase O método de teste deve começar com test O método de teste deve ter um nome que diz o que estamos testando

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -6,12 +6,17 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private $maiorValor = 0;
+    private $maiorValor = -INF; //constante -INF recebe o menor valore possível
+    private $menorValor = INF; //constante INF recebe o maior valor possível
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) { //se este lance for maior que o maiorValor...
-                $this->maiorValor = $lance->getValor();
+                $this->maiorValor = $lance->getValor(); //pega o maior lance e armazena ele na variável maioValor
+            }
+
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
             }
         }
     }
@@ -19,5 +24,10 @@ class Avaliador
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase
 {
-    public function testUm()
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemCrescente()
     {
         //ESQUELETO DE UM TESTE AUTOMATIZADO:
 
@@ -36,7 +36,7 @@ class AvaliadorTest extends TestCase
 
     }
 
-    public function testDois()
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemDecrescente()
     {
         //ESQUELETO DE UM TESTE AUTOMATIZADO:
 
@@ -60,6 +60,56 @@ class AvaliadorTest extends TestCase
         //O PHP unit faz a verificação para mim
         self::assertEquals(2500, $maiorValor); //valida se 2 valores são iguais, espera o valor esperado e quem armazena o outro valor. é uma função estática
 
+    }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
+    {
+        //ESQUELETO DE UM TESTE AUTOMATIZADO:
+
+        //Arrumo a casa para o teste (Arrange ou Given)
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+
+        //Executo o código a ser testado (Act ou When)
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        //Verifico se a saída é a esperada (Assert ou Then)
+        //O PHP unit faz a verificação para mim
+        self::assertEquals(2000, $menorValor); //valida se 2 valores são iguais, espera o valor esperado e quem armazena o outro valor. é uma função estática
+    }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
+    {
+        //ESQUELETO DE UM TESTE AUTOMATIZADO:
+
+        //Arrumo a casa para o teste (Arrange ou Given)
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+
+        //Executo o código a ser testado (Act ou When)
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        //Verifico se a saída é a esperada (Assert ou Then)
+        //O PHP unit faz a verificação para mim
+        self::assertEquals(2000, $menorValor); //valida se 2 valores são iguais, espera o valor esperado e quem armazena o outro valor. é uma função estática
     }
 
 }
