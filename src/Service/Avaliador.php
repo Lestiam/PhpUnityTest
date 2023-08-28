dafe02e3f840113355a50b6fd8e02744b5b3184a
IgorTelesCaetano <igor-t-c@hotmail.com>
2023-08-28 10:03:10 -0300

    acabou

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,12 +2,14 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private $maiorValor = -INF; //constante -INF recebe o menor valore possível
     private $menorValor = INF; //constante INF recebe o maior valor possível
+    private $maioresLances;
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
@@ -19,6 +21,12 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances(); //pego os lances do leilão
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance1->getValor() - $lance2->getValor(); //se o lance 1 precisar vir primeiro na lista, eu coloco um número negativo, se o lance 2 precisa vir primeiro na lista, eu retorno positivo e se empatar, eu retorno 0
+    }); //função usort que recebe como parametro o array que quero ordenar  e uma função de ordenação. Iss ordenada para mim
+        $this->maioresLances = array_slice($lances, 0 ,3); //pego os maiores lances e uso a função array_slice para fatir o array no primeiro parametro, o segundo parametro indica de onde eu quero começar a pegar e o terceiro parametro, quantos valores do array quero pegar
     }
 
     public function getMaiorValor(): float
@@ -29,5 +37,13 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /**
+     * @return Lance []
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
